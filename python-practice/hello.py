56310e41f54dace24a20fd30a2489dd1febe4353
wllgood <wuwinnie5@gnail.com>
2025-09-06 23:47:17 -0400

    Updated notes for hello.py

--- python-practice/hello.py
+++ python-practice/hello.py
@@ -2,10 +2,11 @@
 
 print ("Hello world!")
 print ("What is your name?") # ask for user's name
-myName = input()
-print ("It is good to meet you, " + myName)
+myName = input() # input() function waits for user input; creates myName variable to store input
+
+print ("It is good to meet you, " + myName) # call myName variable to print user's name
 print ("The length of your name is:")
-print (len(myName))
+print (len(myName)) # len() function returns length of string; it evaluates to an integer
 print ("What is your age?") # ask for user's age
-myAge = input()
+myAge = input() # store user's age in myAge variable
 print ("You will be " + str(int(myAge) + 1) + " in a year.")
