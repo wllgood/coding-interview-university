b83b5dc7cacc850eb4a9a2abd97adcadd88bdf32
wllgood <wuwinnie5@gnail.com>
2025-09-06 23:26:32 -0400

    Added content to hello.py

--- python-practice/hello.py
+++ python-practice/hello.py
@@ -1,1 +1,11 @@
-print ("Hello, Git Bash and VS Code!")
+# This program prints "Hello world!" to the console and asks for user's name.
+
+print ("Hello world!")
+print ("What is your name?") # ask for user's name
+myName = input()
+print ("It is good to meet you, " + myName)
+print ("The length of your name is:")
+print (len(myName))
+print ("What is your age?") # ask for user's age
+myAge = input()
+print ("You will be " + str(int(myAge) + 1) + " in a year.")
